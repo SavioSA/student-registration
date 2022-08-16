17d8b30d1c23bfa7eb615358a7905e9e3c043741
savio.araujo <savio.araujo@exedio.com.br>
2022-08-16 09:11:55 -0300

    feat(dto): update dtos

--- apps/api/dto/create-course.dto.ts
+++ apps/api/dto/create-course.dto.ts
@@ -1,12 +1,12 @@
 import { IsString, MaxLength, MinLength } from 'class-validator';
 class CreateCourseDto {
   @IsString()
-  @MaxLength(50, { message: "Name must be a maximum of 50 characters" })
-  @MinLength(3, { message: "Name must be a mininum of 50 characters" })
+  @MaxLength(50, { message: "Description must be a maximum of 50 characters" })
+  @MinLength(3, { message: "Description must be a mininum of 3 characters" })
   description: string;
 
   @IsString()
-  @MinLength(3, { message: "Name must be a mininum of 50 characters" })
+  @MinLength(3, { message: "Menu must be a mininum of 3 characters" })
   menu: string;
 }
 
--- apps/api/dto/create-student.dto.ts
+++ apps/api/dto/create-student.dto.ts
@@ -2,7 +2,7 @@ import { IsString, MaxLength, MinLength } from 'class-validator';
 class CreateStudentDto {
   @IsString()
   @MaxLength(50, { message: "Name must be a maximum of 50 characters" })
-  @MinLength(3, { message: "Name must be a mininum of 50 characters" })
+  @MinLength(3, { message: "Name must be a mininum of 3 characters" })
   name: string;
 }
 
