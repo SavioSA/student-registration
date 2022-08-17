649f510263bad6439c1cdb1a527e49eb74f83893
saviosa <saviosilvadearaujo2019@gmail.com>
2022-08-17 10:01:06 -0300

    feat(front): implement side nav menu

--- apps/student-registration/src/app/app.module.ts
+++ apps/student-registration/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { MaterialModule } from './material.module';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, SideMenuComponent],
   imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ apps/student-registration/src/app/components/side-menu/side-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'student-registration-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: ['./side-menu.component.scss'],
+})
+export class SideMenuComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
