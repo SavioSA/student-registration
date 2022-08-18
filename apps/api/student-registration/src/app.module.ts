15feb2c07b26847254436f9f052076c9ae6e1201
saviosa <saviosilvadearaujo2019@gmail.com>
2022-08-18 20:44:57 -0300

    feat(api): remove unused controller

--- apps/api/student-registration/src/app.module.ts
+++ apps/api/student-registration/src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { CourseController } from './controllers/course/course.controller';
+import { CourseService } from './controllers/course/course.service';
 import { StudentController } from './controllers/student/student.controller';
 import { StudentService } from './controllers/student/student.service';
 import { DatabaseModule } from './database/database.module';
-import { CourseController } from './controllers/course/course.controller';
-import { CourseService } from './controllers/course/course.service';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [AppController, StudentController, CourseController],
+  controllers: [StudentController, CourseController],
   providers: [StudentService, CourseService],
 })
 export class AppModule {}
